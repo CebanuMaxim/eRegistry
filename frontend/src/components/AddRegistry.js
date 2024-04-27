dd600fd3b7e5fe1fae00970388814dc12a8a986a
Maxim <cebanumaxim31@gmail.com>
2024-04-27 14:05:35 +0300

    '

--- frontend/src/components/AddRegistry.js
+++ frontend/src/components/AddRegistry.js
@@ -7,21 +7,6 @@ import Button from 'react-bootstrap/Button'
 import * as yup from 'yup'
 import { dateFormatToMD } from '../utils/formatDateHandler'
 
-const isValidCalendarDay = (value) => {
-  // Parse the date string into day, month, and year components
-  const [day, month, year] = value.split('.').map(Number)
-
-  // Create a Date object and set the day, month, and year
-  const date = new Date(year, month - 1, day) // Month is zero-based in JavaScript Date object
-
-  // Check if the parsed date components match the original values and the date object is valid
-  return (
-    date.getDate() === day && // Ensure day matches the original value
-    date.getMonth() + 1 === month && // Ensure month matches the original value
-    date.getFullYear() === year // Ensure year matches the original value
-  )
-}
-
 const DATE_FORMAT_REGEX =
   /^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(19|20)\d{2}$/
 
