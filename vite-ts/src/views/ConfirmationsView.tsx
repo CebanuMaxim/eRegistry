7404488ade588137f3ad000ff473c22af82d1ec6
Maxim <cebanumaxim31@gmail.com>
2024-10-28 15:43:50 +0200

    '

--- vite-ts/src/views/ConfirmationsView.tsx
+++ vite-ts/src/views/ConfirmationsView.tsx
@@ -6,6 +6,7 @@ import { Document, PDFViewer, Font } from '@react-pdf/renderer'
 import RobotoLight from '../fonts/Roboto-Light.ttf'
 import RobotoRegular from '../fonts/Roboto-Regular.ttf'
 import RobotoMedium from '../fonts/Roboto-Medium.ttf'
+import { Act } from '../types'
 
 Font.register({
   family: 'RobotoLight',
@@ -23,6 +24,34 @@ Font.register({
 const Confirmations = () => {
   const { filteredActs } = useContext(FilteredActsContext)
   const { typographyId, registryId } = useParams()
+
+  type Item = {
+    id: number
+    name: string
+    value: number
+  }
+
+  const sumDuplicatesByProperty = <T extends Record<string, any>>(
+    array: T[],
+    keyToCheck: keyof T,
+    keyToSum: keyof T
+  ): T[] => {
+    const counts: Record<string, T> = {}
+
+    array.forEach((item) => {
+      const id = item[keyToCheck] as string
+      if (!counts[id]) {
+        counts[id] = { ...item }
+      } else {
+        // Ensure the value to sum is a number
+        counts[id][keyToSum] =
+          (counts[id][keyToSum] as number) + (item[keyToSum] as number)
+      }
+    })
+
+    return Object.values(counts)
+  }
+  console.log(sumDuplicatesByProperty(array, 'id', 'value'))
 
   return (
     <PDFViewer style={{ width: '100%', height: window.innerHeight }}>
